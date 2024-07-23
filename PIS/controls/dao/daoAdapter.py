2c0ab43d888a823f958e26d68d524effeab89fb2
Darwin Granda <darwin.granda@unl.edu.ec>
2024-07-23 06:05:19 -0500

    conflict

--- PIS/controls/dao/daoAdapter.py
+++ PIS/controls/dao/daoAdapter.py
@@ -14,7 +14,7 @@ class DaoAdapter(Generic[T]):
     def __init__(self, atype: T):
         self.atype = atype
         conexion = Connection()
-        self.conn = conexion.connect("USUARIO_DBA_PIS", "2003", "XE")
+        self.conn = conexion.connect("USUARIO_DBA", "2001", "XE")
       
       
     def _list(self) -> T:
